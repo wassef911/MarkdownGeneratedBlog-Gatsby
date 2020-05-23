fcbc7535777d234011658175a132d7a5d728b173
wassef911 <1.wassef911@gmail.com>
2020-05-23 06:27:28 +0100

    added styled components

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,38 @@
-exports.onCreateNode = ({ node, actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkDownRemark`) {
+    const slug = createNodeField({ node, getNode })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allMarkDownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({node}))=>{
+      createPage({
+        path : node.fields.slug
+      })
+    }
+  }
+
+  )
 }
